25825bc2cc185c2dbc1ce4bb85305d92e39ad3a3
Alejandro García Cortado <alejandrogarciacortado@gmail.com>
2018-11-20 10:34:49 +0100

    Cambios antes del Merge

--- app/src/main/java/com/marca/folcloricas/Inicio.java
+++ app/src/main/java/com/marca/folcloricas/Inicio.java
@@ -1,60 +1,23 @@
 package com.marca.folcloricas;
 
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Inicio extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private VideoView videoBG;
-    MediaPlayer mMediaPlayer;
-    int mCurrentVideoPosition;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.inicio);
 
-
-        videoBG = (VideoView) findViewById(R.id.videoView_main);
-
-        // Build your video Uri
-        Uri uri = Uri.parse("android.resource://"+ getPackageName() + "/" + R.raw.background);
-
-        videoBG.setVideoURI(uri);
-
-
-        videoBG.start();
-
-        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mMediaPlayer = mediaPlayer;
-                mMediaPlayer.setVolume(0f, 0f);
-
-                // We want our video to play over and over so we set looping to true.
-                mMediaPlayer.setLooping(true);
-                // We then seek to the current posistion if it has been set and play the video.
-                if (mCurrentVideoPosition != 0) {
-
-                    mMediaPlayer.seekTo(mCurrentVideoPosition);
-                    mMediaPlayer.start();
-                }
-            }
-        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -73,25 +36,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     }
-    protected void onPause() {
-        super.onPause();
-        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
-        videoBG.pause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        videoBG.start();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mMediaPlayer.release();
-        mMediaPlayer = null;
-    }
-
 
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
